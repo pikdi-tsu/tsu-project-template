f119f5ee13d588c582a5659db4b6eae0a94b554b
Bertha <bertha@tsu.ac.id>
2025-11-10 10:35:13 +0700

    Inisialisasi project SIAKAD

---
+++ sources/bootstrap/cache/system_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\System\\Providers\\SystemServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\System\\Providers\\SystemServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
